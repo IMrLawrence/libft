b17c80036217641d5191a1a3d17c85d5067ec74a
Mr. Lawrence <75909919+IMrLawrence@users.noreply.github.com>
2022-03-25 00:55:14 -0300

    ft_memcpy function

--- Makefile
+++ Makefile
@@ -8,7 +8,8 @@ SRCS	:=	ft_isalpha.c \
 			ft_toupper.c \
 			ft_tolower.c \
 			ft_bzero.c	 \
-			ft_memset.c
+			ft_memset.c  \
+			ft_memcpy.c
 OBJS	:=	$(SRCS:.c=.o)
 
 CC		:=	cc
--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,28 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wsoares- <wsoares-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/23 18:36:03 by wsoares-          #+#    #+#             */
-/*   Updated: 2022/03/24 15:47:00 by wsoares-         ###   ########.fr       */
+/*   Created: 2022/03/24 15:39:43 by wsoares-          #+#    #+#             */
+/*   Updated: 2022/03/25 00:54:20 by wsoares-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# include <stddef.h>
+#include <stddef.h>
 
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isascii(int c);
-int		ft_isalnum(int c);
-int		ft_isprint(int c);
-int		ft_toupper(int c);
-int		ft_tolower(int c);
-void	ft_bzero(void *s, size_t n);
-void	*ft_memset(void *s, int c, size_t n);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	const unsigned char	*tmp_src;
+	unsigned char		*tmp_dest;
+	size_t				counter;
 
-#endif
+	tmp_dest = dest;
+	tmp_src = src;
+	counter = 0;
+	while (counter < n)
+	{
+		*(tmp_dest + counter) = *(tmp_src + counter);
+		counter++;
+	}
+	return (tmp_dest);
+}
