1b72e70bb6f9e036a659df3405568b54a0661d7f
Mr. Lawrence <75909919+IMrLawrence@users.noreply.github.com>
2022-04-18 16:39:10 -0300

    ft_lstdelone function

--- Makefile
+++ Makefile
@@ -37,7 +37,8 @@ SRCS_B		:=	ft_lstnew.c			\
 				ft_lstadd_front.c	\
 				ft_lstsize.c		\
 				ft_lstlast.c		\
-				ft_lstadd_back.c
+				ft_lstadd_back.c	\
+				ft_lstdelone.c
 
 OBJS		:=	$(SRCS:.c=.o)
 OBJS_B		:=	$(SRCS_B:.c=.o)
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: wsoares- <wsoares-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 18:36:03 by wsoares-          #+#    #+#             */
-/*   Updated: 2022/04/18 12:37:02 by wsoares-         ###   ########.fr       */
+/*   Updated: 2022/04/18 15:08:51 by wsoares-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,5 +60,6 @@ void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
 
 #endif
