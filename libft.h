1ab0ae5a18cf2ab258bac29f031bfe6874313db0
Mr. Lawrence <75909919+IMrLawrence@users.noreply.github.com>
2022-04-19 07:05:13 -0300

    ft_lstiter function

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: wsoares- <wsoares-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 18:36:03 by wsoares-          #+#    #+#             */
-/*   Updated: 2022/04/18 16:40:21 by wsoares-         ###   ########.fr       */
+/*   Updated: 2022/04/19 06:48:59 by wsoares-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,5 +62,6 @@ t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
 
 #endif
