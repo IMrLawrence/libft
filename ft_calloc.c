c7be845648fca9ce843c57c9c33b4eb2fb679e21
Mr. Lawrence <75909919+IMrLawrence@users.noreply.github.com>
2022-04-21 20:15:44 -0300

    ft_calloc correct conditional

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,11 +6,12 @@
 /*   By: wsoares- <wsoares-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 21:35:38 by wsoares-          #+#    #+#             */
-/*   Updated: 2022/03/28 23:27:38 by wsoares-         ###   ########.fr       */
+/*   Updated: 2022/04/21 19:59:35 by wsoares-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <stdint.h>
 
 void	ft_bzero(void *s, size_t n);
 
@@ -18,7 +19,7 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*mem;
 
-	if (nmemb == 0 || size == 0)
+	if ((nmemb * size) == (SIZE_MAX * SIZE_MAX))
 		return (NULL);
 	mem = malloc(nmemb * size);
 	if (mem != NULL)
